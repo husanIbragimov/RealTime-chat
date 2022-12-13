d0f2e85070e10a10c5c47e4e8cbaabbd535bf099
Husan Ibragimov <ibragmovhusan283@gmail.com>
2022-12-13 11:20:39 +0500

    create chat models

--- apps/chats/models.py
+++ apps/chats/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
-# Create your models here.
 
+class Chat(models.Model):
+    room_name = models.CharField(max_length=221)
+
+    def __str__(self):
+        return self.room_name
+
+    def get_messages(self):
+        pass
